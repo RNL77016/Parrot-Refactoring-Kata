b78789bf7fa941ec016ce1ead45320975ace0f2c
RNL77016 <rnl77016@lasallebajio.edu.mx>
2026-05-17 23:55:34 -0600

    refactor: extract NorwegianBlueParrot subclass

---
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -0,0 +1,22 @@
+package parrot;
+
+public class NorwegianBlueParrot extends Parrot {
+    private final double voltage;
+    private final boolean isNailed;
+    
+    public NorwegianBlueParrot(double voltage, boolean isNailed) {
+        super(ParrotTypeEnum.NORWEGIAN_BLUE, 0, voltage, isNailed);
+        this.voltage = voltage;
+        this.isNailed = isNailed;
+    }
+    
+    @Override
+    public double getSpeed() {
+        return isNailed ? 0 : getBaseSpeed(voltage);
+    }
+    
+    @Override
+    public String getCry() {
+        return voltage > 0 ? "Bzzzzzz" : "...";
+    }
+}
